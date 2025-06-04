cbcd8caca161e79fd00663874d25957b71d9dec6
Midouni Billel <midounibillel@gmail.com>
2025-06-04 21:12:16 +0200

    docs: update user model comments

--- core/models/User.ts
+++ core/models/User.ts
@@ -1,7 +1,7 @@
-// User class model 
-// add create method to create a new user in the database/auth service
-// add get method to get a user from the database/auth service
-// add update method to update a user in the database/auth service
+/**
+ * Simple user model used by the application.
+ * It exposes helper methods that talk to the authentication service.
+ */
 
 import { register } from '../services/Auth';
 
@@ -14,14 +14,25 @@ class User {
     this.password = password;
   }
 
+  /**
+   * Registers the user with the underlying authentication service.
+   */
   async create() {
     await register(this.email, this.password);
   }
 
+  /**
+   * Retrieves the user information from the authentication service.
+   * Currently not implemented.
+   */
   get() {
     // get user from database/auth service
   }
 
+  /**
+   * Updates the user information in the authentication service.
+   * Currently not implemented.
+   */
   update() {
     // update user in database/auth service
   }
